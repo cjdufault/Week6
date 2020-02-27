454d49f109bdd5287bf83313c15d9af96c207ca5
Connor Dufault <mx8434an@go.minnstate.edu>
2020-02-27 12:26:58 -0600

    added a final message box that displays the correct answers and the user's answers

--- Quiz/Form1.cs
+++ Quiz/Form1.cs
@@ -18,6 +18,7 @@ namespace Quiz
             { "Microsoft owns Github", true },
             { "100 is the ASCII code for \"A\"", false }
         };
+        private SortedList<string, bool> PlayerAnswers = new SortedList<string, bool>(); // contains player's answers, to be displayed at the end
         private int QuestionNumber = -1;
         private int Score = 0;
 
@@ -54,7 +55,7 @@ namespace Quiz
             {
                 btnCheckAnswer.Enabled = false;
                 btnCheckAnswer.Text = "Quiz over!";
-                MessageBox.Show($"Your score is {Score}", "Quiz over!");
+                ShowResults();
                 this.Dispose();
             }
         }
@@ -65,6 +66,16 @@ namespace Quiz
             {
                 KeyValuePair<string, bool> question = Questions.ElementAt(QuestionNumber);
                 bool correctAnswer = question.Value;
+
+                // add the question and the player's answer to the PlayerAnswers sorted list
+                if (rdoTrue.Checked)
+                {
+                    PlayerAnswers.Add(question.Key, true);
+                }
+                else if (rdoFalse.Checked)
+                {
+                    PlayerAnswers.Add(question.Key, false);
+                }
 
                 // compare the answer to the radio button that has been clicked
                 if (correctAnswer == true & rdoTrue.Checked == true)
@@ -78,6 +89,23 @@ namespace Quiz
 
                 lblScore.Text = $"Score: {Score}";
             }
+        }
+
+        private void ShowResults()
+        {
+            string results = "";
+
+            // build a string that shows the correct answer and the player's answer to each question
+            foreach(KeyValuePair<string, bool> question in Questions)
+            {
+                results += $"\n{question.Key}";
+                results += $"\nCorrect answer:{question.Value}";
+                results += $"\nYour answer: {PlayerAnswers[question.Key]}\n";
+            }
+
+            results += $"\nFinal Score: {Score}"; // add the final score
+
+            MessageBox.Show(results, "Final Results"); // show that string in a message box
         }
     }
 }
