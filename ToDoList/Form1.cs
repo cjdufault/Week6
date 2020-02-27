e322baa7f50f5a47d27e4bb05b06b104c9acc6bc
Connor Dufault <mx8434an@go.minnstate.edu>
2020-02-27 10:31:17 -0600

    Add project files.

---
+++ ToDoList/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToDoList
+{
+    public partial class Form1 : Form
+    {
+        List<string> toDoList= new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddToDo_Click(object sender, EventArgs e)
+        {
+            string newToDo = txtNewToDo.Text;
+            toDoList.Add(newToDo);
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
