9df895a3feba49217c7534a8d95657c43be81e27
Connor Dufault <mx8434an@go.minnstate.edu>
2020-02-27 12:08:10 -0600

    added quiz app

---
+++ Quiz/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Quiz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtQuestion = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.rdoTrue = new System.Windows.Forms.RadioButton();
+            this.rdoFalse = new System.Windows.Forms.RadioButton();
+            this.btnCheckAnswer = new System.Windows.Forms.Button();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtQuestion
+            // 
+            this.txtQuestion.Location = new System.Drawing.Point(29, 34);
+            this.txtQuestion.Name = "txtQuestion";
+            this.txtQuestion.ReadOnly = true;
+            this.txtQuestion.Size = new System.Drawing.Size(255, 20);
+            this.txtQuestion.TabIndex = 0;
+            this.txtQuestion.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.rdoFalse);
+            this.panel1.Controls.Add(this.rdoTrue);
+            this.panel1.Location = new System.Drawing.Point(29, 74);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(255, 69);
+            this.panel1.TabIndex = 1;
+            // 
+            // rdoTrue
+            // 
+            this.rdoTrue.AutoSize = true;
+            this.rdoTrue.Location = new System.Drawing.Point(15, 14);
+            this.rdoTrue.Name = "rdoTrue";
+            this.rdoTrue.Size = new System.Drawing.Size(47, 17);
+            this.rdoTrue.TabIndex = 0;
+            this.rdoTrue.Text = "True";
+            this.rdoTrue.UseVisualStyleBackColor = true;
+            // 
+            // rdoFalse
+            // 
+            this.rdoFalse.AutoSize = true;
+            this.rdoFalse.Location = new System.Drawing.Point(15, 38);
+            this.rdoFalse.Name = "rdoFalse";
+            this.rdoFalse.Size = new System.Drawing.Size(50, 17);
+            this.rdoFalse.TabIndex = 1;
+            this.rdoFalse.Text = "False";
+            this.rdoFalse.UseVisualStyleBackColor = true;
+            // 
+            // btnCheckAnswer
+            // 
+            this.btnCheckAnswer.Location = new System.Drawing.Point(93, 163);
+            this.btnCheckAnswer.Name = "btnCheckAnswer";
+            this.btnCheckAnswer.Size = new System.Drawing.Size(126, 23);
+            this.btnCheckAnswer.TabIndex = 2;
+            this.btnCheckAnswer.Text = "Check Answer";
+            this.btnCheckAnswer.UseVisualStyleBackColor = true;
+            this.btnCheckAnswer.Click += new System.EventHandler(this.btnCheckAnswer_Click);
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.Location = new System.Drawing.Point(119, 212);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(69, 17);
+            this.lblScore.TabIndex = 3;
+            this.lblScore.Text = "Score = 0";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnCheckAnswer;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(315, 287);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.btnCheckAnswer);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.txtQuestion);
+            this.Name = "Form1";
+            this.Text = "Tech Quiz";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtQuestion;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton rdoFalse;
+        private System.Windows.Forms.RadioButton rdoTrue;
+        private System.Windows.Forms.Button btnCheckAnswer;
+        private System.Windows.Forms.Label lblScore;
+    }
+}
+
---
+++ Quiz/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quiz
+{
+    public partial class Form1 : Form
+    {
+        private readonly SortedList<string, bool> Questions = new SortedList<string, bool>()
+        {
+            { "SSDs are faster than HDDs", true },
+            { "Microsoft owns Github", true },
+            { "100 is the ASCII code for \"A\"", false }
+        };
+        private int QuestionNumber = -1;
+        private int Score = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ShowNextQuestion(); // show the 1st question
+        }
+
+        private void btnCheckAnswer_Click(object sender, EventArgs e)
+        {
+            if (rdoTrue.Checked || rdoFalse.Checked)
+            {
+                CheckAnswer();
+                ShowNextQuestion();
+            }
+        }
+
+        // advances to the next question, and ends the game if questions are exhausted
+        private void ShowNextQuestion()
+        {
+            QuestionNumber++;
+
+            if (QuestionNumber < Questions.Count)
+            {
+                KeyValuePair<string, bool> question = Questions.ElementAt(QuestionNumber);
+                string questionText = question.Key;
+                txtQuestion.Text = questionText;
+
+                rdoTrue.Checked = false;
+                rdoFalse.Checked = false;
+            }
+            else
+            {
+                btnCheckAnswer.Enabled = false;
+                btnCheckAnswer.Text = "Quiz over!";
+                MessageBox.Show($"Your score is {Score}", "Quiz over!");
+                this.Dispose();
+            }
+        }
+
+        private void CheckAnswer()
+        {
+            if (QuestionNumber < Questions.Count)
+            {
+                KeyValuePair<string, bool> question = Questions.ElementAt(QuestionNumber);
+                bool correctAnswer = question.Value;
+
+                // compare the answer to the radio button that has been clicked
+                if (correctAnswer == true & rdoTrue.Checked == true)
+                {
+                    Score++;
+                }
+                if (correctAnswer == false & rdoFalse.Checked == true)
+                {
+                    Score++;
+                }
+
+                lblScore.Text = $"Score: {Score}";
+            }
+        }
+    }
+}
