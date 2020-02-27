1f2c4ef166065c1ea347475b4e5868f5c8852638
Connor Dufault <mx8434an@go.minnstate.edu>
2020-02-27 13:32:04 -0600

    added waterbill app

---
+++ WaterBill/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace WaterBill
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtQ1 = new System.Windows.Forms.TextBox();
+            this.txtQ2 = new System.Windows.Forms.TextBox();
+            this.txtQ3 = new System.Windows.Forms.TextBox();
+            this.txtQ4 = new System.Windows.Forms.TextBox();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.txtAvg = new System.Windows.Forms.TextBox();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Quarter";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Second Quarter";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Third Quarter";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(32, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Fourth Quarter";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(267, 88);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Total";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(251, 115);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Average";
+            // 
+            // txtQ1
+            // 
+            this.txtQ1.Location = new System.Drawing.Point(126, 31);
+            this.txtQ1.Name = "txtQ1";
+            this.txtQ1.Size = new System.Drawing.Size(88, 20);
+            this.txtQ1.TabIndex = 6;
+            // 
+            // txtQ2
+            // 
+            this.txtQ2.Location = new System.Drawing.Point(126, 58);
+            this.txtQ2.Name = "txtQ2";
+            this.txtQ2.Size = new System.Drawing.Size(88, 20);
+            this.txtQ2.TabIndex = 7;
+            // 
+            // txtQ3
+            // 
+            this.txtQ3.Location = new System.Drawing.Point(126, 85);
+            this.txtQ3.Name = "txtQ3";
+            this.txtQ3.Size = new System.Drawing.Size(88, 20);
+            this.txtQ3.TabIndex = 8;
+            // 
+            // txtQ4
+            // 
+            this.txtQ4.Location = new System.Drawing.Point(126, 111);
+            this.txtQ4.Name = "txtQ4";
+            this.txtQ4.Size = new System.Drawing.Size(88, 20);
+            this.txtQ4.TabIndex = 9;
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(304, 85);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.ReadOnly = true;
+            this.txtTotal.Size = new System.Drawing.Size(100, 20);
+            this.txtTotal.TabIndex = 10;
+            this.txtTotal.TabStop = false;
+            // 
+            // txtAvg
+            // 
+            this.txtAvg.Location = new System.Drawing.Point(304, 111);
+            this.txtAvg.Name = "txtAvg";
+            this.txtAvg.ReadOnly = true;
+            this.txtAvg.Size = new System.Drawing.Size(100, 20);
+            this.txtAvg.TabIndex = 11;
+            this.txtAvg.TabStop = false;
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(114, 157);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(75, 23);
+            this.btnCalc.TabIndex = 12;
+            this.btnCalc.Text = "Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(113, 34);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(13, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "$";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(113, 61);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(13, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "$";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(113, 88);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(13, 13);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "$";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(113, 114);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(13, 13);
+            this.label10.TabIndex = 16;
+            this.label10.Text = "$";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnCalc;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(448, 203);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.txtAvg);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.txtQ4);
+            this.Controls.Add(this.txtQ3);
+            this.Controls.Add(this.txtQ2);
+            this.Controls.Add(this.txtQ1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Average Quarterly Bill";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtQ1;
+        private System.Windows.Forms.TextBox txtQ2;
+        private System.Windows.Forms.TextBox txtQ3;
+        private System.Windows.Forms.TextBox txtQ4;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.TextBox txtAvg;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ WaterBill/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace WaterBill
+{
+    public partial class Form1 : Form
+    {
+        private List<double> quarters = new List<double>(); // list that contains values for all quarters
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            if (ParseQuarterFields()) 
+            {
+                // add up all of the values
+                double total = 0;
+                foreach (double value in quarters)
+                {
+                    total += value;
+                }
+
+                double avg = total / quarters.Count; // calculate average
+
+                txtTotal.Text = total.ToString("c");
+                txtAvg.Text = avg.ToString("c");
+            }
+        }
+
+        // returns true if parsing was successful
+        private bool ParseQuarterFields()
+        {
+            try
+            {
+                double q1Bill = Double.Parse(txtQ1.Text);
+                double q2Bill = Double.Parse(txtQ2.Text);
+                double q3Bill = Double.Parse(txtQ3.Text);
+                double q4Bill = Double.Parse(txtQ4.Text);
+
+                quarters.Add(q1Bill);
+                quarters.Add(q2Bill);
+                quarters.Add(q3Bill);
+                quarters.Add(q4Bill);
+
+                return true;
+            }
+            // catch fields w/o a number inputted
+            catch (FormatException)
+            {
+                MessageBox.Show("At least one field does not contain a number.", "Error");
+                return false;
+            }
+            // fields with too large of a number
+            catch (OverflowException)
+            {
+                MessageBox.Show("At least one value is too large.", "Error");
+                return false;
+            }
+        }
+    }
+}
